06cba70ae3fb552a78c6caa8059b429a210b98cd
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-01-25 01:59:01 +0900

    [feat] Add enums

---
+++ TAMINGO/Common/Enum/DatePickerType.swift
@@ -0,0 +1,15 @@
+//
+//  DatePickerType.swift
+//  TAMINGO
+//
+//  Created by 김도연 on 1/25/26.
+//
+
+enum SheetType: Identifiable {
+    case date
+    case startTime
+    case endTime
+    case repeatType
+    
+    var id: Self { self }
+}
