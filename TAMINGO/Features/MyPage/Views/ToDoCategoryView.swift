0ed9f6c7944c508fdc846f07a1a5f91427f6f1b5
e0ng <aliceywkwon@naver.com>
2026-02-02 23:27:18 +0900

    [Feat] 카테고리 색상 enum 추가

---
+++ TAMINGO/Features/MyPage/Views/ToDoCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoCategoryView.swift
+//  TAMINGO
+//
+//  Created by 권예원 on 2/2/26.
+//
+
+import SwiftUI
+
+struct ToDoCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoCategoryView()
+}
