055fd7e3d58c6c2b4075bf3ef226a43af63cc948
Doyeon Kim <ehdus022191@dgu.ac.kr>
2026-02-12 07:07:11 +0900

    [Fix] 경고 제거

--- TAMINGO/Features/CalendarView/ViewModels/AddSchedule/AddScheduleViewModel.swift
+++ TAMINGO/Features/CalendarView/ViewModels/AddSchedule/AddScheduleViewModel.swift
@@ -70,7 +70,6 @@ class AddScheduleViewModel {
         didSet {
             // 토글을 켜면 날짜를 오늘로 리셋 (혹은 기존 날짜 유지)
             if isEndDated {
-                let calendar = Calendar.current
                 self.repeatEndDate = Date()
             }
         }
